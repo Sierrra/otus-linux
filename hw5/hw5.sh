7aa9f560acc4276b533b91dc352f782e6f0e5181
Sergey Cheparukhin <sergey.cheparukhin@datanerds.ai>
2019-01-23 16:55:09 +0300

    hw5:final

--- hw5/hw5.sh
+++ hw5/hw5.sh
@@ -18,5 +18,6 @@ echo -e "Errors\n" >> /tmp/cron_log_parse_stat_$from_$to.log
 awk '(($9+0.0) > 400)' access.log >> /tmp/cron_log_parse_stat_$from_$to.log
 echo -e "Response code stats\n" >> /tmp/cron_log_parse_stat_$from_$to.log
 awk '{print $9}' access.log | sort | uniq -c | sort -rn >> /tmp/cron_log_parse_stat_$from_$to.log
+cat /tmp/cron_log_parse_stat_$from_$to.log | mail -s "Stats from $from to $to" email@test.me
 rm -f $LOCK
 trap - INT TERM EXIT
